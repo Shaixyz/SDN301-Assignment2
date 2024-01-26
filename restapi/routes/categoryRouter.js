1ff0479f47bf145e62f9d80f39f1422b0aab2aac
Kiet <kietquang240703@gmail.com>
2024-01-26 23:31:58 +0700

    UI 10%

--- restapi/routes/categoryRouter.js
+++ restapi/routes/categoryRouter.js
@@ -5,7 +5,7 @@ const categoryRouter = express.Router()
 categoryRouter.use(bodyParser.json()) 
 
 categoryRouter.route('/')
-.get((res,req)=>{
+.get((req, res)=>{
     Categories.find({})
     .then((categories)=>{
         res.statusCode = 200
@@ -13,4 +13,48 @@ categoryRouter.route('/')
         res.json(categories)
     })
 })
+.post((req, res)=>{
+    Categories.create(req.body)
+    .then((category)=>{
+        res.json(category);
+    })
+})
+.put((req, res) => {
+    res.end('No support');
+})
+.delete((req, res) => {
+    Categories.deleteOne({})
+        .then((resp) => {
+            res.json(resp);
+        })
+});
+categoryRouter.route('/:categoryId')
+    .get((req, res) => {
+        Categories.findById(req.params.categoryId)
+            .then((category) => {
+                res.json(category);
+            })
+    })
+    .put((req, res) => {
+        Categories.findByIdAndUpdate(req.params.categoryId, {
+            $set: req.body
+        }, { new: true })
+            .then((category) => {
+                res.json(category);
+            })
+    })
+    .delete((req, res) => {
+        Categories.findOneAndDelete({ _id: req.params.categoryId })
+            .then((response) => {
+                if (response) {
+                    res.json(response);
+                } else {
+                    res.status(404).json({ error: 'Category not found' });
+                }
+            })
+            .catch((error) => {
+                res.status(500).json({ error: error.message });
+            });
+    });
+
 module.exports = categoryRouter
--- restapi/routes/orchidRouter.js
+++ restapi/routes/orchidRouter.js
@@ -1,55 +1,25 @@
-const express = require('express');
-const Orchids = require('../models/orchids');
+const express = require('express')
 const bodyParser = require('body-parser');
+
+const Orchids = require('../models/orchids');
+const orchidController = require('../controllers/orchidController');
+
 const orchidRouter = express.Router();
+
 orchidRouter.use(bodyParser.json());
 
-orchidRouter.route('/')
-    .get((req, res) => {
-        Orchids.find({})
-            .then((orchids) => {
-                // res.statusCode = 200;
-                // res.setHeader('Content-Type', 'application/json');
-                // res.json(orchids);
-                res.render('orchids', {orchids: orchids})
-            })
-    })
-    .post((req, res) => {
-        Orchids.create(req.body)
-            .then((orchid) => {
-                res.json(orchid);
-            })
-    })
-    .put((req, res) => {
-        res.end('No support');
-    })
-    .delete((req, res) => {
-        Orchids.deleteOne({})
-            .then((resp) => {
-                res.json(resp);
-            })
-    });
 
-orchidRouter.route('/:orchidId')
-    .get((req, res) => {
-        Orchids.findById(req.params.orchidId)
-            .then((orchid) => {
-                res.json(orchid);
-            })
-    })
-    .put((req, res) => {
-        Orchids.findByIdAndUpdate(req.params.orchidId, {
-            $set: req.body
-        }, { new: true })
-            .then((orchid) => {
-                res.json(orchid);
-            })
-    })
-    .delete((req, res) => {
-        Orchids.findByIdAndRemove(req.params.orchidId)
-            .then((resq) => {
-                res.json(resq);
-            })
-    });
+orchidRouter
+    .route('/')
+    .get(orchidController.index)
+    .post(orchidController.create) 
+orchidRouter
+   .route('/delete/:orchidId')   
+   .get(orchidController.remove)
+orchidRouter
+   .route('/edit/:orchidId')
+   .get(orchidController.formEdit)
+   .post(orchidController.edit)
 
+   
 module.exports = orchidRouter;
