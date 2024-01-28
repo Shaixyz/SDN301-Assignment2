a94471bfc64b5a3433535469fe7e5b5e6a073820
Kiet <kietquang240703@gmail.com>
2024-01-28 10:09:31 +0700

    UI 99%

--- restapi/routes/orchidRouter.js
+++ restapi/routes/orchidRouter.js
@@ -1,25 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const orchidRouter = express.Router();
+orchidRouter.use(bodyParser.json());
 
 const Orchids = require('../models/orchids');
 const orchidController = require('../controllers/orchidController');
 
-const orchidRouter = express.Router();
-
-orchidRouter.use(bodyParser.json());
-
-
 orchidRouter
-    .route('/')
-    .get(orchidController.index)
-    .post(orchidController.create) 
+   .route('/')
+   .get(orchidController.index)
+   .post(orchidController.create)
 orchidRouter
-   .route('/delete/:orchidId')   
+   .route('/delete/:orchidId')
    .get(orchidController.remove)
 orchidRouter
    .route('/edit/:orchidId')
    .get(orchidController.formEdit)
    .post(orchidController.edit)
 
-   
 module.exports = orchidRouter;
